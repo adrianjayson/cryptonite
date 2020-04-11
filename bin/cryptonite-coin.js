d561313f6e0f087606caa4d587968a8c9226bef5
AJ Catambay <adrianjayson13@gmail.com>
2020-04-11 15:30:16 +0800

    Implements verbose option. Fixes error handling.

--- bin/cryptonite-coin.js
+++ bin/cryptonite-coin.js
@@ -7,7 +7,7 @@ program
     .option('--coin <string>', 'Choose a cryptocurrency, separated by commas for multiple values', 'BTC,ETH,XRP')
     .option('--cur <currency>', 'Change the currency', 'USD')
     .option('--int <string>', 'Interval of data (available 1h,1d,7d,30d,1y,ytd)', '1d')
-    .option('--verbose <boolean>', 'Includes additional data (works when asking for single cryptocurrency only).', false)
+    .option('--verbose <boolean>', 'Includes additional data. Only works when asking for a single cryptocurrency.', false)
     .action((options) => coin.update(options));
 
 program.parse(process.argv);
